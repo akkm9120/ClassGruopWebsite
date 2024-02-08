5b71b1db85a93738404e75fcf98eaec20ca4470c
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-02-08 07:17:49 +0000

    cannout push to json bin yet

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 
 BASE_API ="https://api.jsonbin.io/v3/b";
-BIN_ID = "65c33452dc74654018a16c1a/latest";
+BIN_ID = "65c33452dc74654018a16c1a";
 MASTER_KEY = "$2a$10$JW0IiZb7G0Zz2VbHsYNoW.SjITUv.wcScj6VdQgYlqvjt/FjlUk5q";
 
 function addFood(foodArray,foodname,fooddescription) {
@@ -14,22 +14,14 @@ function addFood(foodArray,foodname,fooddescription) {
 }
 
 async function saveFoods(foods) {
-    
-    console.log("route hit")
-    const response = await axios.put(`${BASE_API}/${BIN_ID}`, foods, {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Master-Key": MASTER_KEY
-      }
-    });
-    console.log("response is here => ", response.data);
-    return response.data;
-  
+ await axios.post(BASE_API+"/"+BIN_ID,foods
 }
 
-async function loadTasks() {
 
-    const response = await axios.get(BASE_API + "/" + BIN_ID + "/latest");
-    return response.data.record;
+
+
+async function loadFoods() {
+  const response = await axios.get(BASE_API + "/" + BIN_ID + "/latest");
+  return response.data.record;
 }
 
