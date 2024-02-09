e94450d3af0a44ffc7c4a98ff50f785dd463c418
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-02-09 04:02:55 +0000

    added customized bootstrap

--- data.js
+++ data.js
@@ -3,21 +3,29 @@ BASE_API ="https://api.jsonbin.io/v3/b";
 BIN_ID = "65c33452dc74654018a16c1a";
 MASTER_KEY = "$2a$10$JW0IiZb7G0Zz2VbHsYNoW.SjITUv.wcScj6VdQgYlqvjt/FjlUk5q";
 
-function addFood(foodArray,foodname,fooddescription) {
+
+
+
+function addFood(foodArray,foodname,food_description) {
 
     let newFood = {
       "foodname": foodname, 
-      "description": fooddescription
+      "description": food_description
     };
     foodArray.push(newFood);
     
 }
 
-async function saveFoods(foods) {
- await axios.post(BASE_API+"/"+BIN_ID,foods
+
+async function saveFoods(foods){
+const response = await axios.put(`${BASE_API}/${BIN_ID}`, foods, {
+  headers: {
+    "Content-Type": "application/json",
+    "X-Master-Key": MASTER_KEY
+  }
+});
+return response.data;
 }
-
-
 
 
 async function loadFoods() {
--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let foods = [];
 
 
-  
-document.querySelector("#btnMenuAdd").addEventListener("click", function (event){
+document.addEventListener("DOMContentLoaded",async ()=>{
+    let foods =  await loadFoods();
 
-    event.preventDefault();
-    
-    console.log("route hit at btn menu add event listener");
-
-    const food_name = document.querySelector("#txtFoodname").value;
-    const food_description = document.querySelector("#txtDescription").value;
-
-    addFood(foods,food_name,food_description);
+    let menu = document.querySelector("#menu");
     console.log(foods);
-    saveFoods(foods);
-
+        for(let food of foods){
+        menu.innerHTML+= `
+        <div class="card col-12 col-sm-4 col-md-3">
+        <div class="card-body">
+            <h5 class="card-title">${food.foodname}</h5>
+            <p class="card-text">${food.description}</p>
+            <button type="button" class="btn btn-success">Edit</button>
+            <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+        </div>
+`;
+    }
 })
