bc5ea28a5f26bb7b0f36a1018339f15867c1a949
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-02-12 06:21:07 +0000

    working on delete

--- data.js
+++ data.js
@@ -12,13 +12,16 @@ function addFood(foodArray,foodname,food_description) {
       "foodname": foodname, 
       "description": food_description
     };
+    console.log("this is new food =>",newFood);
     foodArray.push(newFood);
-    
+    console.log("this is food array=>",foodArray)
+    return foodArray;   
 }
 
 
 async function saveFoods(foods){
-const response = await axios.put(`${BASE_API}/${BIN_ID}`, foods, {
+  console.log("save food route hit")
+  const response = await axios.put(`${BASE_API}/${BIN_ID}`, foods, {
   headers: {
     "Content-Type": "application/json",
     "X-Master-Key": MASTER_KEY
@@ -30,6 +33,43 @@ return response.data;
 
 async function loadFoods() {
   const response = await axios.get(BASE_API + "/" + BIN_ID + "/latest");
+  console.log("this is load food =>", response.data.record)
   return response.data.record;
+}
+
+function renderFood(foods){
+
+  let menu = document.querySelector("#menu");
+  menu.innerHTML = "";
+  for(let food of foods){
+      menu.innerHTML+= `
+      <div class="card col-12 col-sm-4 col-md-3">
+      <h5 class="card-header">${food.foodname}</h5>
+          <div class="card-body">
+              <p class="card-text">${food.description}</p>
+              <button  class="btn btn-success btn_edit">Edit</button>
+              <button  class="btn btn-danger btn_delete">Delete</button>
+          </div>
+      </div>
+  `; 
+  } menu.querySelector(".btn_delete").addEventListener("click",()=>{
+    console.log("btn delete was clicked")
+   deleteFood(foods, food.foodname);
+  })
+
+  
+  function deleteFood(foods, foodname){
+    let wantedIndex = null;
+    for (let i = 0; i < foods.length ; i++) {
+        if (foodname == foods[i].foodname) {
+            wantedIndex = i;
+        }
+    }
+    if (wantedIndex) {
+        foods.splice(wantedIndex, 1);
+    }
+    saveFoods(foods);
+renderFood(foods);
+}
 }
 
--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-
-
+let foods = []; 
 document.addEventListener("DOMContentLoaded",async ()=>{
-    let foods =  await loadFoods();
+    
+    foods =  await loadFoods();
 
-    let menu = document.querySelector("#menu");
-    console.log(foods);
-        for(let food of foods){
-        menu.innerHTML+= `
-        <div class="card col-12 col-sm-4 col-md-3">
-        <div class="card-body">
-            <h5 class="card-title">${food.foodname}</h5>
-            <p class="card-text">${food.description}</p>
-            <button type="button" class="btn btn-success">Edit</button>
-            <button type="button" class="btn btn-danger">Delete</button>
-        </div>
-        </div>
-`;
-    }
+    console.log("after load foods in DOMcontentloaded", foods)
+    renderFood(foods);
+   
+})
+
+document.querySelector("#btnMenuAdd").addEventListener("click",()=>{
+
+    const food_name = document.querySelector("#txtFoodname").value;
+    const food_description = document.querySelector("#txtDescription").value;
+
+    let new_foodarray =addFood(foods,food_name,food_description);
+    saveFoods(new_foodarray); 
+    renderFood(foods);
 })
