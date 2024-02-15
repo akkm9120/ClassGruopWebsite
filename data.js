308e4140a6a9c35db9a5d466bc6f3c74212bde62
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-02-15 08:42:49 +0000

    adding edit feature

--- data.js
+++ data.js
@@ -7,8 +7,9 @@ MASTER_KEY = "$2a$10$JW0IiZb7G0Zz2VbHsYNoW.SjITUv.wcScj6VdQgYlqvjt/FjlUk5q";
 
 
 function addFood(foodArray,foodname,food_description) {
-
+ let index = foodArray.length ; 
     let newFood = {
+      "index": index,
       "foodname": foodname, 
       "description": food_description
     };
@@ -37,39 +38,4 @@ async function loadFoods() {
   return response.data.record;
 }
 
-function renderFood(foods){
-
-  let menu = document.querySelector("#menu");
-  menu.innerHTML = "";
-  for(let food of foods){
-      menu.innerHTML+= `
-      <div class="card col-12 col-sm-4 col-md-3">
-      <h5 class="card-header">${food.foodname}</h5>
-          <div class="card-body">
-              <p class="card-text">${food.description}</p>
-              <button  class="btn btn-success btn_edit">Edit</button>
-              <button  class="btn btn-danger btn_delete">Delete</button>
-          </div>
-      </div>
-  `; 
-  } menu.querySelector(".btn_delete").addEventListener("click",()=>{
-    console.log("btn delete was clicked")
-   deleteFood(foods, food.foodname);
-  })
-
-  
-  function deleteFood(foods, foodname){
-    let wantedIndex = null;
-    for (let i = 0; i < foods.length ; i++) {
-        if (foodname == foods[i].foodname) {
-            wantedIndex = i;
-        }
-    }
-    if (wantedIndex) {
-        foods.splice(wantedIndex, 1);
-    }
-    saveFoods(foods);
-renderFood(foods);
-}
-}
 
--- script.js
+++ script.js
@@ -1,19 +1,81 @@
-let foods = []; 
-document.addEventListener("DOMContentLoaded",async ()=>{
-    
-    foods =  await loadFoods();
+let foods = [];
+document.addEventListener("DOMContentLoaded", async () => {
+
+    foods = await loadFoods();
 
     console.log("after load foods in DOMcontentloaded", foods)
     renderFood(foods);
-   
+
 })
 
-document.querySelector("#btnMenuAdd").addEventListener("click",()=>{
+document.querySelector("#btnMenuAdd").addEventListener("click", () => {
 
     const food_name = document.querySelector("#txtFoodname").value;
     const food_description = document.querySelector("#txtDescription").value;
 
-    let new_foodarray =addFood(foods,food_name,food_description);
-    saveFoods(new_foodarray); 
+    let new_foodarray = addFood(foods, food_name, food_description);
+    saveFoods(new_foodarray);
     renderFood(foods);
 })
+
+
+
+
+
+
+
+function renderFood(foods) {
+
+    let menu = document.querySelector("#menu");
+    menu.innerHTML = "";
+    for (let food of foods) {
+        menu.innerHTML += `
+        <div class="card col-12 col-sm-4 col-md-3">
+        <h5 class="card-header">${food.foodname}</h5>
+            <div class="card-body">
+                <p class="card-text">${food.description}</p>
+                <button  class="btn btn-success edit-btn">Edit</button>
+                <button  class="btn btn-danger del-btn" value="${food.index}">Delete</button>
+            </div>
+        </div>
+    `;
+        document.querySelectorAll(".del-btn").forEach(delbutton => {
+            delbutton.addEventListener("click", function () {
+                alert(`button ${delbutton.value} is clicked`);
+                foods.splice(delbutton.value, 1);
+                saveFoods(foods);
+                renderFood(foods);
+            })
+        })
+        document.querySelectorAll(".edit-btn").forEach(editbutton => {
+            editbutton.addEventListener("click", function () {
+                let found = false;
+                const indextoUpdate = food.index;
+                for (let i = 0; i < foods.length; i++) {
+                    if (foods[i].index == indextoUpdate) {
+                        found = true;
+                        document.querySelector("#txtFoodname").value = foods[i].name;
+                        document.querySelector("#txtDescription").value =foods[i].description;
+                        document.querySelector("#btnMenuAdd").style.display = 'none';
+                        document.querySelector("#menuControl").appendChild(document.createElement('BUTTON').innerHTML="Save");
+                       let  editedItem = {
+                       "index": food.index,
+                       "foodname": foodname, 
+                       "description": food_description
+                       }
+                        foods.splice(indextoUpdate, 0, editedItem);
+                        saveFoods(foods);
+                        renderFood(foods);
+                    }
+                }
+
+
+            })
+        })
+    }
+}
+
+
+
+
+
